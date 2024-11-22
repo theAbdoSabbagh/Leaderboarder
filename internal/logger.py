547e03cb0b3bfdadeb37543ec1b3b5bf40c4e237
theAbdoSabbagh <abdulghani.alaa.sabbagh@gmail.com>
2024-11-22 16:46:19 +0400

    added new params

--- internal/logger.py
+++ internal/logger.py
@@ -5,21 +5,21 @@ class Logger:
     def __init__(self):
         pass
 
-    def success(self, text: str):
-        print(f"[bold white][{strftime('%H:%M:%S')}][/][bold green][ SUCCESS ][/] [bold white]{text}[/]")
+    def success(self, text: str, start_new: bool = False, end_new: bool = False):
+        print(f"{'\n' if start_new else ''}[bold white][{strftime('%H:%M:%S')}][/][bold green][ SUCCESS ][/] [bold white]{text}[/]{'\n' if end_new else ''}")
     
-    def error(self, text: str):
-        print(f"[bold white][{strftime('%H:%M:%S')}][/][bold red][ ERROR ][/] [bold white]{text}[/]")
+    def error(self, text: str, start_new: bool = False, end_new: bool = False):
+        print(f"{'\n' if start_new else ''}[bold white][{strftime('%H:%M:%S')}][/][bold red][ ERROR ][/] [bold white]{text}[/]{'\n' if end_new else ''}")
 
-    def info(self, text: str):
-        print(f"[bold white][{strftime('%H:%M:%S')}][/][bold blue][ INFO ][/] [bold white]{text}[/]")
+    def info(self, text: str, start_new: bool = False, end_new: bool = False):
+        print(f"{'\n' if start_new else ''}[bold white][{strftime('%H:%M:%S')}][/][bold blue][ INFO ][/] [bold white]{text}[/]{'\n' if end_new else ''}")
 
-    def warning(self, text: str):
-        print(f"[bold white][{strftime('%H:%M:%S')}][/][bold yellow][ WARNING ][/] [bold white]{text}[/]")
+    def warning(self, text: str, start_new: bool = False, end_new: bool = False):
+        print(f"{'\n' if start_new else ''}[bold white][{strftime('%H:%M:%S')}][/][bold yellow][ WARNING ][/] [bold white]{text}[/]{'\n' if end_new else ''}")
 
-    def debug(self, text: str):
-        print(f"[bold white][{strftime('%H:%M:%S')}][/][bold grey][ DEBUG ] {text}[/]")
+    def debug(self, text: str, start_new: bool = False, end_new: bool = False):
+        print(f"{'\n' if start_new else ''}[bold white][{strftime('%H:%M:%S')}][/][bold grey][ DEBUG ] {text}[/]{'\n' if end_new else ''}")
     
-    def question(self, text: str):
-        print(f"[bold white][{strftime('%H:%M:%S')}][/][bold cyan][ QUESTION ][/] [bold white]{text}[/]", end = " ")
+    def question(self, text: str, start_new: bool = False):
+        print(f"{'\n' if start_new else ''}[bold white][{strftime('%H:%M:%S')}][/][bold cyan][ QUESTION ][/] [bold white]{text}[/]", end = " ")
         return input()
